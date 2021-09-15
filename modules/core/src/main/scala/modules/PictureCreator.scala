7e391fb8c1e561d76e7a1d8a4458e55ca3eb7d99
cosmir17 <ks1537@gmail.com>
2021-09-15 02:08:57 +0100

    refactoring the drawer code

--- modules/core/src/main/scala/modules/PictureCreator.scala
+++ modules/core/src/main/scala/modules/PictureCreator.scala
@@ -15,16 +15,16 @@ object PictureCreator {
         for(i<-0 until height + 2) { arr(i)(0) = "|"; arr(i)(width + 1) = "|" }
         for(j<-0 until width + 2) { arr(0)(j) = "-"; arr(height + 1)(j) = "-" }
         arr
-      case (LineCmd(x1, y1, x2, y2), Some(c)) if (x1 == x2 || y1 == y2) && c.length > y1 && c.length > y2 && c(0).length > x1 && c(0).length > x2 =>
+      case (LineCmd(x1, y1, x2, y2), Some(c)) if (x1 == x2 || y1 == y2) && checkIfWithinCanvas(x1, y1, x2, y2, c) => //this is for a horizontal or a vertical line
         for(i <- y1 to y2; j <- x1 to x2) { c(i)(j) = "x" }
         c
-      case (LineCmd(x1, y1, x2, y2), Some(c)) if c.length > y1 && c.length > y2 && c(0).length > x1 && c(0).length > x2 =>
-        for {(i, j) <- y1 to y2 zip (x1 to x2)} {c(i)(j) = "x"}
+      case (LineCmd(x1, y1, x2, y2), Some(c)) if checkIfWithinCanvas(x1, y1, x2, y2, c) => //this is for a diagonal line
+        for { (i, j) <- (y1 to y2) zip (x1 to x2) } { c(i)(j) = "x" } //zip is to iterate over two arrays (diagonal line: width and height are same)
         c
-      case (RectangleCmd(x1, y1, x2, y2), Some(c)) if c.length > y1 && c.length > y2 && c(0).length > x1 && c(0).length > x2  =>
+      case (RectangleCmd(x1, y1, x2, y2), Some(c)) if checkIfWithinCanvas(x1, y1, x2, y2, c)  =>
         for(i <- y1 to y2; j <- x1 to x2) { if(i == y1 || i == y2 || j == x1 || j == x2) {c(i)(j) = "x"} }
         c
-      case (BucketFillCmd(x, y, colour), Some(c)) if c.length > y && c(0).length > x =>
+      case (BucketFillCmd(x, y, colour), Some(c)) if checkIfWithinCanvas(x, y, c) =>
         val xyDefaultColour = c(y)(x); detectSameTilePaint(c, y, x, xyDefaultColour, colour)
         c
       case (_, Some(_)) => throw new IllegalArgumentException("Please provide a suitable canvas that meets the requirements for your command")
@@ -38,4 +38,10 @@ object PictureCreator {
     val xu = c(y)(x + 1); if (xu == fromColour) detectSameTilePaint(c, y, x+1, fromColour, toColour)
     val xd = c(y)(x - 1); if (xd == fromColour) detectSameTilePaint(c, y, x-1, fromColour, toColour)
   }
+
+  private def checkIfWithinCanvas(x1: Int, y1: Int, x2: Int, y2: Int, c: Canvas): Boolean =
+    c.length > y1 && c.length > y2 && c(0).length > x1 && c(0).length > x2
+
+  private def checkIfWithinCanvas(x: Int, y: Int, c: Canvas): Boolean =
+    c.length > y && c(0).length > x
 }
